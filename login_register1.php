46ceea57fb44d2a3799848bc423851a8494ce1fe
Vivekksharma21 <118636483+Vivekksharma21@users.noreply.github.com>
2023-06-24 08:42:51 +0530

    Update login_register1.php

--- login_register1.php
+++ login_register1.php
@@ -1,3 +1,5 @@
+#code is written by Vivek sharma. contact number 7979015053..
+#mail id : vivekrsashok2003@gmail.com
 <?php
 require_once "connection1.php";
 session_start();
@@ -174,7 +176,8 @@ if($_SERVER['REQUEST_METHOD']=="POST")
 
 ?>
 
-
+#code is written by Vivek sharma. contact number 7979015053..
+#mail id : vivekrsashok2003@gmail.com
 
 
     
